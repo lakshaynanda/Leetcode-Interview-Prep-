a109e92470748bcd3f2ee62249412a188d5fb976
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2021-11-09 12:57:23 -0800

    Time: 21 ms (94.16%), Space: 44 MB (74.59%) - LeetHub

--- random-pick-with-weight/random-pick-with-weight.java
+++ random-pick-with-weight/random-pick-with-weight.java
@@ -18,10 +18,10 @@ class Solution {
         int right = cumulative.length - 1;
         while (left < right) {
             int mid = left + (right - left)/2;
-            if (cumulative[mid] < index) {
-                left = mid + 1;
-            } else {
+            if (cumulative[mid] >= index) {
                 right = mid;
+            } else {
+                left = mid + 1;
             }
         }
         return left;
