f36a5dadb2139b61633737549037a86bf9fec4f5
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2021-11-10 11:20:07 -0800

    Time: 10 ms (72.49%), Space: 41.7 MB (59.42%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -8,16 +8,17 @@ class Solution {
         for(int num: nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
         
-        for(int n: map.keySet()) {
-            maxHeap.add(n);
-            if (maxHeap.size() > k) {
+        for(int num: map.keySet()) {
+            maxHeap.add(num);
+            if(maxHeap.size() > k) {
                 maxHeap.poll();
             }
         }
-        int res[] = new int[k];
-        for(int i = k - 1; i >= 0; i--) {
+        int[] res = new int[k];
+        for(int i = 0; i < res.length; i++) {
             res[i] = maxHeap.poll();
         }
         return res;
