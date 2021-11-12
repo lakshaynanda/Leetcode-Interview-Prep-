21c7815c2c8b88aefae2ca416998fd8a674b7c08
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2021-11-12 11:56:04 -0800

    Time: 8 ms (72.47%), Space: 55 MB (45.28%) - LeetHub

---
+++ dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.java
@@ -0,0 +1,27 @@
+class SparseVector {
+    HashMap<Integer, Integer> mapping;
+    SparseVector(int[] nums) {
+        mapping = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                mapping.put(i, nums[i]);
+            }
+        }
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0;
+        for(Integer i : this.mapping.keySet()) {
+            if(vec.mapping.containsKey(i)) {
+                result += vec.mapping.get(i) * this.mapping.get(i);
+            }
+        }
+        return result;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
