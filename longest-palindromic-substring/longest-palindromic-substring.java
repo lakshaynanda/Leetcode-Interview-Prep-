741b48c2fbe9424468bccaf8ac9ab32f53f10315
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2021-12-14 20:45:23 -0800

    Time: 305 ms (20.42%), Space: 43 MB (29.58%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,33 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
-        boolean dp[][] = new boolean[s.length()][s.length()];
         int x = 0;
         int y = 0;
-        int count = 0; 
+        
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        
         for(int gap = 0; gap < s.length(); gap++) {
             for(int i = 0, j = gap; j < s.length(); j++, i++) {
-                if (gap == 0) {
+                if(gap == 0) {
                     dp[i][j] = true;
-                } else if (gap == 1) {
-                    if(s.charAt(i) == s.charAt(j)) {
-                        dp[i][j] = true;
-                    } else {
-                        dp[i][j] = false;
-                    }
                 } else {
-                    if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] == true) {
-                        dp[i][j] = true;
+                    if(gap == 1) {
+                        if(s.charAt(i) == s.charAt(j)) {
+                            dp[i][j] = true;
+                        } else {
+                            dp[i][j] = false;
+                        }
                     } else {
-                        dp[i][j] = false;
+                        if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                            dp[i][j] = true;
+                        } else {
+                            dp[i][j] = false;
+                        }
                     }
                 }
-                if(dp[i][j]) {
-                    count++;
+                if (dp[i][j]) {
                     x = i;
                     y = j;
                 }
             }
+            
         }
-        return s.substring(x,y + 1);
+        return s.substring(x, y + 1);
     }
 }
