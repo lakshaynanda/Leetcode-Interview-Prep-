efc6dad0d20a4bfe34223da02fe762810c7e67e0
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2021-12-17 14:03:22 -0800

    Time: 1 ms (69.38%), Space: 37.5 MB (32.83%) - LeetHub

---
+++ 2-keys-keyboard/2-keys-keyboard.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSteps(int n) {
+        if(n == 1) {
+            return 0;
+        }
+        int ans = 0;
+        int d = 2;
+        while(n > 1) {
+            while(n % d == 0) {
+                ans += d;
+                n = n / d;
+            }
+            d++;
+        }
+        return ans;
+    }
+}
