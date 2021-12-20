80acd8538d8ec64d17622f8e1840788acee1331e
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2021-12-19 22:57:04 -0800

    Time: 2 ms (37.27%), Space: 36.9 MB (96.07%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        char []ch = s.toCharArray();
+        st.push(ch[0]);
+        for(int i = 1; i < ch.length; i++) {
+            if(!st.isEmpty() && st.peek() == '(' && ch[i] == ')') {
+                st.pop();
+            } else if (!st.isEmpty() && st.peek() == '[' && ch[i] == ']') {
+                st.pop();
+            } else if (!st.isEmpty() && st.peek() == '{' && ch[i] == '}') {
+                st.pop();
+            } else {
+                st.push(ch[i]);
+            }
+        }
+        if(st.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
