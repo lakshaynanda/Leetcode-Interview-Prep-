9b1ab3682213697dfd5732258f758abc6e3a03e4
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-03 21:01:30 -0800

    Time: 2 ms (10.30%), Space: 38.6 MB (24.24%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -1,22 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     Map<Integer, Integer> depths = new HashMap<>();
     Map<Integer, Integer> parents = new HashMap<>();
-    
     public boolean isCousins(TreeNode root, int x, int y) {
-        setDepthAndParent(root, x, y, 0, root.val);
+        depthFinder(root, x, y, 0, root.val);
         return (depths.get(x) == depths.get(y)) && (parents.get(x) != parents.get(y));
     }
-    
-    private void setDepthAndParent(TreeNode node, int x, int y, int depth, int parent){
-        if (node == null)
+    private void depthFinder(TreeNode node, int x, int y, int depth, int parent) {
+        if (node == null) {
             return;
-        
-		// No need to iterate rest of the tree if both are found
-        if (depths.containsKey(x) && depths.containsKey(y))
+        }
+        if (depths.containsKey(x) && depths.containsKey(y)){
             return;
-        
-		// Set depth and parent when found
-        if (node.val == x){
+        }
+        if (node.val == x) {
             depths.put(x, depth);
             parents.put(x, parent);
         }
@@ -26,8 +37,9 @@ class Solution {
             parents.put(y, parent);
         }
         
-		// If not found, increase the depth and set the current node value as parent value
-        setDepthAndParent(node.left, x, y, depth+1, node.val);
-        setDepthAndParent(node.right, x, y, depth+1, node.val);
+        depthFinder(node.left, x, y, depth + 1, node.val);
+        depthFinder(node.right, x, y, depth + 1, node.val);
     }
+    
 }
+
