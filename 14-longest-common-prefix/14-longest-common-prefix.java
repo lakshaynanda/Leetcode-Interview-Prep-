32f5f2038244f94aa3f5334286307dde4aa622ca
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-04 17:59:31 -0800

    Time: 2 ms (38.54%), Space: 39.1 MB (21.33%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0 || strs == null) {
+            return "";
+        }
+        return helperdnc(strs, 0, strs.length - 1);
+    }
+    public String helperdnc(String[] strs, int left, int right) {
+        if (left == right) {
+            return strs[left];
+        } else {
+            int mid = left + (right - left) / 2;
+            String lp = helperdnc(strs, left, mid);
+            String rp = helperdnc(strs, mid + 1, right);
+            
+            return commonPart(lp, rp);
+        }
+    }
+    public String commonPart(String lp, String rp) {
+        int min = Math.min(lp.length(), rp.length());
+        for(int i = 0; i < min; i++) {
+            if (lp.charAt(i) != rp.charAt(i)) {
+                return lp.substring(0, i);
+            }
+        }
+        return lp.substring(0, min);
+    }
+}
