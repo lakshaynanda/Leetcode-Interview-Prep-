1385a6f80b996d412de7ea82f45f54ce8b5d89fa
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-06 15:07:30 -0800

    Time: 8 ms (22.05%), Space: 37.5 MB (24.85%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -12,14 +12,18 @@ class Solution {
         //     it1 = sum;
         // }
         // return sum;
+        // if (n <= 1) {
+        //     return n;
+        // }
+        // int []dp = new int[n + 1];
+        // dp[1] = 1;
+        // for(int i = 2; i <= n; i++) {
+        //     dp[i] = dp[i - 1] + dp[i - 2]; 
+        // }
+        // return dp[n];
         if (n <= 1) {
             return n;
         }
-        int []dp = new int[n + 1];
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2]; 
-        }
-        return dp[n];
+        return fib(n - 1) + fib(n - 2);
     }
 }
