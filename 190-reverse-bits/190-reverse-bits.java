bee5669061f7ed04d585f671ac053ad94f685c9b
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-12 09:47:41 -0800

    Time: 1 ms (98.89%), Space: 38.7 MB (67.07%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -3,6 +3,7 @@ public class Solution {
     public int reverseBits(int n) {
         int ans = 0;
         int rev = 0;
+        
         for(int i = 0; i < 32; i++) {
             ans = ans << 1;
             ans += (n & 1);
