2a11e78b8f95fe370e979e687434768578187f8d
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-16 14:36:33 -0800

    Time: 800 ms (8.82%), Space: 53.5 MB (27.91%) - LeetHub

--- 370-range-addition/370-range-addition.java
+++ 370-range-addition/370-range-addition.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] getModifiedArray(int length, int[][] updates) {
         int arr[] = new int[length];
-        for(int i = 0; i < updates.length; i++) {
-            getResult(updates[i], arr);
-        }
-        return arr;
-    }
-    public int[] getResult(int[] updates, int[] arr) {
-        for(int i = updates[0]; i <= updates[1]; i++) {
-            arr[i] += updates[2];
+        for(int[] tuple: updates) {
+            int start = tuple[0];
+            int end = tuple[1];
+            int val = tuple[2];
+            for(int i = start; i <= end; i++) {
+                arr[i] += val;
+            }
         }
         return arr;
     }
