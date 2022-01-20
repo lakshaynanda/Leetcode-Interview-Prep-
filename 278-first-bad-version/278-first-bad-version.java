e9453e1fff4e424a711382190859a23d89a9ce35
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-20 14:03:56 -0800

    Time: 28 ms (15.63%), Space: 38.1 MB (9.51%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -7,7 +7,7 @@ public class Solution extends VersionControl {
         int right = n;
         
         while(left <= right) {
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left) / 2;
             if (isBadVersion(mid)) {
                 right = mid - 1;
             } else {
