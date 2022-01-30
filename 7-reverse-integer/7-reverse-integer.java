80ed340a84aa496c5f18e82f78e48489215c1b44
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-30 14:42:38 -0800

    Time: 2 ms (59.81%), Space: 41.3 MB (5.25%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        
+        while (x != 0) {
+            int rem = x % 10;
+            int newres = result * 10 + rem;
+            if ((newres - rem)/10 != result) {
+                return 0;
+            }
+            result = newres;
+            x= x / 10;
+        }
+        return result;
+    }
+}
