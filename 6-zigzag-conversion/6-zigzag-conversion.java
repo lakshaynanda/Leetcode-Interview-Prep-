7ed38636a29669560cc8fb9819b34232178356e0
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-01-30 14:04:52 -0800

    Time: 28 ms (25.24%), Space: 52.3 MB (15.00%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        HashMap<Integer, StringBuilder> map = new HashMap<>();
+        boolean incr = true;
+        int pos = 0;
+        
+        for(char c : s.toCharArray()) {
+            if (pos == numRows) {
+                incr = false;
+            } 
+            if (pos == 1) {
+                incr = true;
+            }
+            if (incr) {
+                pos++;
+            } else {
+                pos--;
+            }
+            if (!map.containsKey(pos)) {
+                map.put(pos, new StringBuilder());
+            }
+            map.get(pos).append(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i : map.keySet()) {
+            sb.append(map.get(i));
+        }
+        return sb.toString();
+    }
+}
