e4bbc326e3ba3b2c4efe73297fa88029baa4c16d
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-08 14:18:22 -0800

    Time: 2 ms (47.24%), Space: 48.3 MB (12.15%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -3,17 +3,17 @@ class Solution {
         int start = 0;
         int end = nums.length - 1;
         
-        while(start < end) {
+        while (start <= end) {
             if (nums[start] % 2 > nums[end] % 2) {
                 int temp = nums[start];
                 nums[start] = nums[end];
                 nums[end] = temp;
-            }
-            if (nums[start] % 2 == 0) {
-                start++;
-            }
-            if(nums[end] % 2 == 1) {
-                end--;
+            } else {
+                if (nums[start] % 2 == 0) {
+                    start++;
+                } else if(nums[start] % 2 == 1) {
+                    end--;
+                }
             }
         }
         return nums;
