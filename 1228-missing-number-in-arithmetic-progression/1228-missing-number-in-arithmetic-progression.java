382ac3648e54438e393838cd18131bceb9be2bb2
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-08 23:35:52 -0800

    Time: 0 ms (100.00%), Space: 43.2 MB (6.34%) - LeetHub

--- 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.java
+++ 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.java
@@ -2,17 +2,17 @@ class Solution {
     public int missingNumber(int[] arr) {
         int n = arr.length;
         
-        int diff = (arr[n - 1] - arr[0]) / n;
         int lo = 0;
-        int hi = n - 1;
+        int high = n - 1;
         
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            
+        int diff = (arr[n - 1] - arr[0]) / n;
+        
+        while (lo <= high) {
+            int mid = (lo + high) / 2;
             if (arr[mid] == arr[0] + mid * diff) {
                 lo = mid + 1;
             } else {
-                hi = mid - 1;
+                high = mid - 1;
             }
         }
         return arr[0] + diff * lo;
