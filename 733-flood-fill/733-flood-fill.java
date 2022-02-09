1cb964e2d270b1d817b5477bf9f755ab16d00cad
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-08 17:59:42 -0800

    Time: 0 ms (100.00%), Space: 42.9 MB (65.32%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -6,7 +6,6 @@ class Solution {
         helper(image, sr, sc, image[sr][sc], newColor);
         return image;
     }
-    
     public void helper(int[][] image, int sr, int sc, int color, int newColor) {
         if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) {
             return;
