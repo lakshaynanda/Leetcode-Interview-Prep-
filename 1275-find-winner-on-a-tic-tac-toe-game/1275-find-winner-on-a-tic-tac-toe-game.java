98b1d292ac1e1108324addaec2cfb5cd89d2b2fd
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-08 21:32:20 -0800

    Time: 0 ms (100.00%), Space: 40.1 MB (16.65%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -1,23 +1,21 @@
 class Solution {
     public String tictactoe(int[][] moves) {
-        Character[][] board = getFilledBoard(moves);
+        Character[][] board = getFilled(moves);
         for(int i = 0; i < 3; i++) {
-            if (board[i][0] != null && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+            if (board[i][0] != null && (board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) {
                 return board[i][0] == 'X' ? "A" : "B";
-            }
-            if (board[0][i] != null && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+            } else if (board[0][i] != null && (board[0][i] == board[1][i]) && (board[1][i] == board[2][i])) {
                 return board[0][i] == 'X' ? "A" : "B";
-            }
-            if(board[1][1] != null && (board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[2][0] == board[1][1] && board[1][1] == board[0][2])) {
+            } else if (board[1][1] != null && (board[1][1] == board[0][0] && board[1][1] == board[2][2] || board[1][1] == board[2][0] && board[1][1] == board[0][2])) {
                 return board[1][1] == 'X' ? "A" : "B";
             }
         }
-        return moves.length == 9 ? "Draw" : "Pending";
+        return moves.length == 9 ? "Draw" : "Pending"; 
     }
-    public Character[][] getFilledBoard(int[][] moves) {
-        Character[][] board = new Character[3][3];
+    public Character[][] getFilled(int[][] moves) {
+        Character board[][] = new Character[3][3];
         for(int i = 0; i < moves.length; i++) {
-            board[moves[i][0]][moves[i][1]] = ((i & 1) == 0) ? 'X' : 'O';
+            board[moves[i][0]][moves[i][1]] = (i & 1) == 0 ? 'X' : 'O';
         }
         return board;
     }
