55d722ae9297868eebb9fea6792b435cbe76b9e7
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-12 01:54:45 -0800

    Time: 0 ms (100.00%), Space: 40.1 MB (31.06%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -3,13 +3,14 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
         
-        for(char c : s.toCharArray()) {
-            if (c == '(') {
+        for(char ch: s.toCharArray()) {
+            if(ch == '(') {
                 stack.push(0);
             } else {
                 int v = stack.pop();
-                int w = stack.pop();
-                stack.push(w + Math.max(2 * v, 1));
+                int u = stack.pop();
+                
+                stack.push(u + Math.max(2*v, 1));
             }
         }
         return stack.pop();
