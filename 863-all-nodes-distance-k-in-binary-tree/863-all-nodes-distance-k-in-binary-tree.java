8f07ae5816e6f62cb12f79059c2cc35555b0776c
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-12 12:27:15 -0800

    Time: 14 ms (84.69%), Space: 43.7 MB (5.39%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -8,46 +8,50 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode, TreeNode> parentmap;
-    List<Integer> ans;
+    HashMap<TreeNode, TreeNode> parentMap;
+    List<Integer> list;
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        parentmap = new HashMap<>();
-        ans = new ArrayList<>();
+        parentMap = new HashMap<>();
+        list = new ArrayList<>();
         createParentMap(root);
         HashSet<TreeNode> visited = new HashSet<>();
-        dfs(target, visited, k);
-        return ans;
+        dfs(visited, target, k);
+        return list;
     }
     public void createParentMap(TreeNode root) {
         if (root == null) {
             return;
         }
-        if(root.left != null) {
-            parentmap.put(root.left, root);
+        if (root.left != null) {
+            parentMap.put(root.left, root);
         }
-        if(root.right != null) {
-            parentmap.put(root.right, root);
+        if (root.right != null) {
+            parentMap.put(root.right, root);
         }
+        
         createParentMap(root.left);
         createParentMap(root.right);
     }
-    public void dfs(TreeNode target, HashSet<TreeNode> visited, int k) {
+    
+    public void dfs(HashSet<TreeNode> visited, TreeNode target, int k) {
         if (target == null) {
             return;
         }
         visited.add(target);
+        
         if (k == 0) {
-            ans.add(target.val);
+            list.add(target.val);
             return;
         }
-        if (parentmap.containsKey(target) && !visited.contains(parentmap.get(target))) {
-            dfs(parentmap.get(target), visited, k - 1);
+        if (parentMap.containsKey(target) && !visited.contains(parentMap.get(target))) {
+            dfs(visited, parentMap.get(target) , k - 1);
         }
         if (!visited.contains(target.left)) {
-            dfs(target.left, visited, k - 1);
+            dfs(visited, target.left , k - 1);
         }
         if (!visited.contains(target.right)) {
-            dfs(target.right, visited, k - 1);
+            dfs(visited, target.right , k - 1);
         }
+        
     }
 }
