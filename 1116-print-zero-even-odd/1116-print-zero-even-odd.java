e73163687b483c7821ef01c72f656edbb9a17f1b
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-12 13:07:44 -0800

    Time: 16 ms (28.59%), Space: 42.5 MB (6.61%) - LeetHub

---
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -0,0 +1,57 @@
+class ZeroEvenOdd {
+    private int n;
+    private int count = 1;
+    private boolean flag = true;
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        while (count <= n) {
+            synchronized(this) {
+                while(!flag) {
+                    wait();
+                }
+                if (count <= n) {
+                    printNumber.accept(0);
+                }
+                flag = false;
+                notifyAll();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        while (count <= n) {
+            synchronized(this) {
+                while(flag || count % 2 != 0) {
+                    wait();
+                }
+                if (count <= n) {
+                    printNumber.accept(count);
+                }
+                
+                flag = true;
+                count++;
+                notifyAll();
+            }
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        while (count <= n) {
+            synchronized(this) {
+                while (flag || count % 2 == 0) {
+                    wait();
+                }
+                if (count <= n) {
+                    printNumber.accept(count);
+                }
+                flag = true;
+                count++;
+                notifyAll();
+            }
+        }
+    }
+}
