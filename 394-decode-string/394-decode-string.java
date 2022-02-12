b74a01beb91bd44f1ed2bb32401a5660f251577b
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-12 15:49:44 -0800

    Time: 3 ms (37.19%), Space: 42.3 MB (8.48%) - LeetHub

---
+++ 394-decode-string/394-decode-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ']') {
+                List<Character> decodedString = new ArrayList<>();
+                while(stack.peek() != '[') {
+                    decodedString.add(stack.pop());
+                }
+                stack.pop();
+                int k = 0;
+                int base = 1;
+                
+                while(!stack.isEmpty() && Character.isDigit(stack.peek())) {
+                    k += (stack.pop() - '0') * base;
+                    base *= 10;
+                }
+                
+                while (k != 0) {
+                    for(int j = decodedString.size() - 1; j >= 0; j--) {
+                        stack.push(decodedString.get(j));
+                    }
+                    k--;
+                }
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        char[] result = new char[stack.size()];
+        for(int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return new String(result);
+    }
+}
