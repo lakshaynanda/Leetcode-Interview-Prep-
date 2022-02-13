1020a6416f2c744a73d593893117eec04eca00cb
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-12 17:24:24 -0800

    Time: 3 ms (84.36%), Space: 50.7 MB (69.72%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -14,10 +14,10 @@ class Solution {
             return 0;
         }
         grid[i][j] = '0';
-        dfs(grid, i + 1 , j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
         dfs(grid, i - 1, j);
-        dfs(grid, i , j + 1);
-        dfs(grid, i , j - 1);
         return 1;
     }
 }
