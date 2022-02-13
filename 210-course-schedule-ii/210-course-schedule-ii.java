4af5032d8300f9ea2dfea74b5a33db197dc6dcce
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-13 09:39:58 -0800

    Time: 17 ms (23.80%), Space: 50.2 MB (9.85%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        int[] topological = new int[numCourses];
+        int[] indegree = new int[numCourses];
+        
+        for(int i = 0; i < prerequisites.length; i++) {
+            int src = prerequisites[i][1];
+            int dest = prerequisites[i][0];
+            
+            List<Integer> list = adjList.getOrDefault(src, new ArrayList<>());
+            list.add(dest);
+            adjList.put(src, list);
+            indegree[dest]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        
+        int i = 0;
+        while (!q.isEmpty()) {
+            int node = q.remove();
+            topological[i++] = node;
+            
+            if (adjList.containsKey(node)) {
+                for(Integer neighbour: adjList.get(node)) {
+                    indegree[neighbour]--;
+                    if(indegree[neighbour] == 0) {
+                        q.add(neighbour);
+                    }
+                }
+                
+            }
+        }
+        if (i == numCourses) {
+            return topological;
+        }
+        return new int[0];
+    }
+}
