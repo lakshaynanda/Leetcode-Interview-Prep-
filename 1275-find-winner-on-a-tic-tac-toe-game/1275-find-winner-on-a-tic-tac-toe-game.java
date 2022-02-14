8cefa70ab65fb812408778c88ee75baa20811045
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-13 17:24:37 -0800

    Time: 1 ms (53.08%), Space: 41.9 MB (12.08%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -15,7 +15,7 @@ class Solution {
     public Character[][] getFilled(int[][] moves) {
         Character board[][] = new Character[3][3];
         for(int i = 0; i < moves.length; i++) {
-            board[moves[i][0]][moves[i][1]] = (i & 1) == 0 ? 'X' : 'O';
+            board[moves[i][0]][moves[i][1]] = (i % 2) == 0 ? 'X' : 'O';
         }
         return board;
     }
