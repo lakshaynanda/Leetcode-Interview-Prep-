48ae7ab6e9d0e1d6d169a7b9ef7233a37f1fc49e
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-02-23 19:11:43 -0800

    Time: 99 ms (93.18%), Space: 110.6 MB (89.20%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,80 +1,53 @@
 class Solution {
-    
-    private Map<Integer, List<Integer>> graph;
-    private Map<Integer, Integer> rank;
-    private Map<Pair<Integer, Integer>, Boolean> connDict;
-    
+    List<List<Integer>> ans;
+	int count = 1;
+
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-       
-        this.formGraph(n, connections);
-        this.dfs(0, 0);
+        ans = new ArrayList<>();
+        List<List<Integer>> edge = new ArrayList<>();
+		for(int i = 1; i <= n; i++) {
+			edge.add(new ArrayList<>());
+		}
+		for(int i = 0; i < connections.size(); i++){
+			int u = connections.get(i).get(0);
+			int v = connections.get(i).get(1);
+			edge.get(u).add(v);
+			edge.get(v).add(u);
+		}
         
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for (Pair<Integer, Integer> criticalConnection : this.connDict.keySet()) {
-            result.add(new ArrayList<Integer>(Arrays.asList(criticalConnection.getKey(), criticalConnection.getValue())));
-        }
+        int[] par = new int[n];
+		par[0] = -1;
+		int[] disc = new int[n];
+		int[] low = new int[n];
+		
+		boolean[] vis = new boolean[n];
+		dfs(0, edge, vis, par, disc, low);
         
-        return result;
+        return ans;
     }
     
-    private int dfs(int node, int discoveryRank) {
-        
-        // That means this node is already visited. We simply return the rank.
-        if (this.rank.get(node) != null) {
-            return this.rank.get(node);
-        }
-        
-        // Update the rank of this node.
-        this.rank.put(node, discoveryRank);
-        
-        // This is the max we have seen till now. So we start with this instead of INT_MAX or something.
-        int minRank = discoveryRank + 1;
-        
-        for (Integer neighbor : this.graph.get(node)) {
-            
-            // Skip the parent.
-            Integer neighRank = this.rank.get(neighbor);
-            if (neighRank != null && neighRank == discoveryRank - 1) {
-                continue;
-            }
-            
-            // Recurse on the neighbor.
-            int recursiveRank = this.dfs(neighbor, discoveryRank + 1);
-            
-            // Step 1, check if this edge needs to be discarded.
-            if (recursiveRank <= discoveryRank) {
-                int sortedU = Math.min(node, neighbor), sortedV = Math.max(node, neighbor);
-                this.connDict.remove(new Pair<Integer, Integer>(sortedU, sortedV));
-            }
-            
-            // Step 2, update the minRank if needed.
-            minRank = Math.min(minRank, recursiveRank);
-        }
-        
-        return minRank;
-    }
-    
-    private void formGraph(int n, List<List<Integer>> connections) {
-        
-        this.graph = new HashMap<Integer, List<Integer>>();
-        this.rank = new HashMap<Integer, Integer>();
-        this.connDict = new HashMap<Pair<Integer, Integer>, Boolean>();
-        
-        // Default rank for unvisited nodes is "null"
-        for (int i = 0; i < n; i++) {
-            this.graph.put(i, new ArrayList<Integer>());
-            this.rank.put(i, null);
-        }
-        
-        for (List<Integer> edge : connections) {
-            
-            // Bidirectional edges
-            int u = edge.get(0), v = edge.get(1);
-            this.graph.get(u).add(v);
-            this.graph.get(v).add(u);
-            
-            int sortedU = Math.min(u, v), sortedV = Math.max(u, v);
-            connDict.put(new Pair<Integer, Integer>(sortedU, sortedV), true);
-        }
-    }
+    public void dfs(int n, List<List<Integer>> edge, boolean[] vis, int[] par, int[] disc, int[] low) {
+		vis[n] = true;
+		disc[n] = count;
+		low[n] = count;
+		count++;
+		
+		for(int nbr : edge.get(n)) {
+			if(par[n] == nbr)
+				continue;
+			else if(vis[nbr] == true)
+				low[n] = Math.min(low[n], disc[nbr]);
+			else {
+				par[nbr] = n;
+				dfs(nbr, edge, vis, par, disc, low);
+				low[n] = Math.min(low[n], low[nbr]);
+				if(low[nbr] > disc[n]) {
+                    List<Integer> toadd = new ArrayList<>();
+                    toadd.add(n);
+                    toadd.add(nbr);
+                    ans.add(toadd);
+				}
+			}
+		}
+	}
 }
