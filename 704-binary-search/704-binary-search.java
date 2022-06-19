7dc2ff9c23441cf346faf17d5b003b3e5571d8f9
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-06-19 12:10:35 -0500

    Time: 0 ms (100.00%), Space: 54.9 MB (15.64%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -8,10 +8,10 @@ class Solution {
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
+            if (nums[mid] <= target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return -1;
