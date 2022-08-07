94d76421f071fd64be331d209bdc9f3f7828b7df
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-08-06 19:45:32 -0500

    Time: 4 ms (77.28%), Space: 42.7 MB (87.13%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -6,9 +6,9 @@ class Solution {
         }
         
         for(int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[] {map.get(complement), i};
+            int val = target - nums[i];
+            if (map.containsKey(val) && map.get(val) != i) {
+                return new int[]{ map.get(val), i };
             }
         }
         return new int[2];
