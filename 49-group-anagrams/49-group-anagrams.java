dbfa9593b6c70fe4d5b7042c5d36c03a803022e7
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-09-15 14:21:10 -0700

    Time: 12 ms (71.71%), Space: 57.4 MB (26.97%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -3,16 +3,23 @@ class Solution {
         if (strs == null || strs.length == 0) {
             return new ArrayList<>();
         }
-        HashMap<String, List<String>> map = new HashMap<>();
+        HashMap<Double, List<String>> map = new HashMap<>();
         for (String s: strs) {
-            char[] c = s.toCharArray();
-            Arrays.sort(c);
-            String sorted = String.valueOf(c);
-            if (!map.containsKey(sorted)) {
-                map.put(sorted, new ArrayList<>());
+            double primeProd = primeProduct(s);
+            if (!map.containsKey(primeProd)) {
+                map.put(primeProd, new ArrayList<>());
             }
-                map.get(sorted).add(s);
+                map.get(primeProd).add(s);
         }
         return new ArrayList<>(map.values());
+    }
+    private double primeProduct(String s) {
+        int[] primes = {2,3 ,5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+        double res = 1;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            res *= primes[c - 'a'];
+        }
+        return res;
     }
 }
