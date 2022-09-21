2f770c2e55d0cb0c4db2dd9b36d7f3bcef9435d0
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-09-20 19:03:00 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (78.64%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -5,13 +5,14 @@ class Solution {
             return 0;
         }
         
-        int dp[][] = new int[n][2];
-        dp[0][1] = nums[0];
+        int skip = 0;
+        int take = nums[0];
         
-        for(int i = 1; i < dp.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            dp[i][1] = dp[i - 1][0] + nums[i];
+        for(int i = 1; i < n; i++) {
+            int temp = skip;
+            skip = Math.max(skip, take);
+            take = temp + nums[i];
         }
-        return Math.max(dp[n - 1][0], dp[n - 1][1]);
+        return Math.max(take, skip);
     }
 }
