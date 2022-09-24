ccd6c356f462d9880695a66dab6a6a36f02ca054
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-09-24 01:38:58 -0700

    Time: 2 ms (10.62%), Space: 42.1 MB (72.28%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,55 +1,47 @@
 class Solution {
+    int m, n;
     public void gameOfLife(int[][] board) {
-
-        // Neighbors array to find 8 neighboring cells for a given cell
-        int[] neighbors = {0, 1, -1};
-
-        int rows = board.length;
-        int cols = board[0].length;
-
-        // Create a copy of the original board
-        int[][] copyBoard = new int[rows][cols];
-
-        // Create a copy of the original board
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                copyBoard[row][col] = board[row][col];
-            }
+        if (board == null || board.length == 0) {
+            return;
         }
-
-        // Iterate through board cell by cell.
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-
-                // For each cell count the number of live neighbors.
-                int liveNeighbors = 0;
-
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-
-                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
-                            int r = (row + neighbors[i]);
-                            int c = (col + neighbors[j]);
-
-                            // Check the validity of the neighboring cell.
-                            // and whether it was originally a live cell.
-                            // The evaluation is done against the copy, since that is never updated.
-                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (copyBoard[r][c] == 1)) {
-                                liveNeighbors += 1;
-                            }
-                        }
+        
+        m = board.length; 
+        n = board[0].length;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int liveNeighbor = countLiveNeighbor(board, i, j);
+                if (board[i][j] == 0) {
+                    if (liveNeighbor == 3) {
+                        board[i][j] = 3;
+                    }
+                } else {
+                    if (liveNeighbor < 2 || liveNeighbor > 3) {
+                        board[i][j] = 2;
                     }
                 }
-
-                // Rule 1 or Rule 3
-                if ((copyBoard[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
-                    board[row][col] = 0;
-                }
-                // Rule 4
-                if (copyBoard[row][col] == 0 && liveNeighbors == 3) {
-                    board[row][col] = 1;
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (board[i][j] == 2) {
+                    board[i][j] = 0;
+                } else if (board[i][j] == 3) {
+                    board[i][j] = 1;
                 }
             }
         }
+    }
+    private int countLiveNeighbor(int[][] board, int i, int j) {
+        int count = 0;
+        int dirs[][] = {{0,1},{0,-1},{1,0},{-1,0},{-1,-1},{-1,1},{1,-1},{1,1}};
+        for(int dir[] : dirs) {
+            int nr = i + dir[0];
+            int nc = j + dir[1];
+            if (nr >= 0 && nc >= 0 && nr < m && nc < n && (board[nr][nc] == 1 || board[nr][nc] == 2)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
