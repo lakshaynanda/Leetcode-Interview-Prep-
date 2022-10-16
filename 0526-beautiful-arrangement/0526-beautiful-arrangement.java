047be58da9e875f8870a569b876e1e5dc4faaf4b
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-10-16 10:19:11 -0700

    Time: 115 ms (26.37%), Space: 40.8 MB (62.92%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -0,0 +1,20 @@
+class Solution {
+    int count = 0;
+    public int countArrangement(int n) {
+        boolean[] visited = new boolean[n + 1];
+        calc(n, 1, visited);
+        return count;
+    }
+    private void calc(int n, int pos, boolean[] visited) {
+        if (pos > n) {
+            count++;
+        }
+        for(int i = 1; i <= n; i++) {
+            if(!visited[i] && (pos % i == 0 || i % pos == 0)) {
+                visited[i] = true;
+                calc(n, pos + 1, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
